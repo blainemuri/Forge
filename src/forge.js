b3df977d95c80c604181596a1c982f1b04593a39
blaine1726 <blainemuri@gmail.com>
2016-09-14 13:29:12 -0700

    translated code into a format that loads the models

--- src/forge.js
+++ src/forge.js
@@ -16,13 +16,51 @@ function onInitialized() {
   console.log(Autodesk);
   viewerApp = new Autodesk.A360ViewingApplication('viewer');
   viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
-  viewerApp.loadDocumentWithItemAndObject(document1);
+  Autodesk.Viewing.Document.load(
+    document1,
+    (model)=> {
+      var rootItem = model.getRootItem();
+
+      var geometryItems3d = Autodesk.Viewing.Document.getSubItemsWithProperties(
+        rootItem, {
+          'type': 'geometry',
+          'role': '3d'},
+      true);
+
+      var domContainer = document.getElementById('viewer');
+
+      var viewer = new Autodesk.Viewing.Private.GuiViewer3D(domContainer);
+      viewer.initialize();
+      viewer.setLightPreset(8);
+
+      var options = {
+        globalOffset: {
+          x: 0, y: 0, z: 0
+        }
+      }
+
+      var options2 = {
+        globalOffset: {
+          x: 1, y: 0, z: 0
+        }
+      }
+
+      var path = geometryItems3d[0];
+
+      var block1 = model.getViewablePath(path);
+      console.log(block1);
+
+      viewer.loadModel(model.getViewablePath(path), options);
+      viewer.loadModel(model.getViewablePath(path), options2);
+    }
+  )
+  // viewerApp.loadDocumentWithItemAndObject(document1);
   // globalOffset = viewerApp.model.getData().globalOffset;
 }
 
-function onDocumentLoaded() {
-  console.log("Document Loaded");
-}
+
+
+// OLD WAY OF DOING THINGS (SIMPLER)
 // Autodesk.Viewing.Initializer(options, function onInitialized(){
 //     viewerApp = new Autodesk.A360ViewingApplication('viewer');
 //     viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
